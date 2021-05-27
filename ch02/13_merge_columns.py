eb4b7c1496d069873aad648544bf2c4ffaebbc58
taro-masuda <taro.masuda.jp@gmail.com>
2021-05-27 15:06:25 +0900

    [mod] modified file name on on 12.

--- ch02/13_merge_columns.py
+++ ch02/13_merge_columns.py
@@ -1,11 +1,10 @@
-import subprocess
 import filecmp
 import pandas as pd
 
-def extract_col(idx_col: int, filepath_in: str) -> str:
+def merge_col(filepath1: str, filepath2: str) -> str:
     df = pd.read_table(filepath_in, header=None)
     df_1col = df.iloc[:, idx_col]
-    filepath_out = filepath_in.replace('.txt', 'col' + str(idx_col+1) + '.txt')
+    filepath_out = filepath_in.replace('.txt', '-12-python-' + str(idx_col) + '.txt')
     df_1col.to_csv(filepath_out, index=False, header=None)
     return filepath_out
 
@@ -13,10 +12,8 @@ def split_columns(filepath_in: str, idx_col: int) -> str:
     return extract_col(idx_col=idx_col, filepath_in=filepath_in)
 
 if __name__ == '__main__':
-    filepath_in = './data/popular-names.txt'
-
-    filepath_out_py = [split_columns(filepath_in=filepath_in, idx_col=0)]
-    filepath_out_py.append(split_columns(filepath_in=filepath_in, idx_col=1))
+    filepath1 = './data/col1.txt'
+    filepath2 = './data/col2.txt'
 
     # cut -f 1 ./data/popular-names.txt > ./data/popular-names-12-unix-0.txt
     # cut -f 2 ./data/popular-names.txt > ./data/popular-names-12-unix-1.txt
