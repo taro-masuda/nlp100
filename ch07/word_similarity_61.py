7c3f117698c2129d509438fb9d8b5112814980fa
taro-masuda <taro.masuda.jp@gmail.com>
2021-06-05 16:30:01 +0900

    [add] added an answer on 61.

--- ch07/word_similarity_61.py
+++ ch07/word_similarity_61.py
@@ -1,4 +1,13 @@
-from load_word_vector_60 import load
+from load_word_vector_60 import WordEmbedding
 
 if __name__ == '__main__':
-    pass
+    filepath = './data/GoogleNews-vectors-negative300.bin'
+    
+    word_embedding = WordEmbedding()
+    word_embedding.load_dataset(filepath)
+
+    similarity = word_embedding.model.similarity('United_States', 'U.S.')
+    print(similarity)
+    '''
+    0.73107743
+    '''
