afcc7c92730971cd565ca66a95a68bb100727249
taro-masuda <taro.masuda.jp@gmail.com>
2021-06-07 18:26:20 +0900

    [mod] modified to save model on 73..

--- ch08/training_sgd_73.py
+++ ch08/training_sgd_73.py
@@ -9,6 +9,7 @@ from single_layer_predict_71 import seed_everything, Net
 if __name__ == "__main__":
     seed_everything()
     filedir_in = './data'
+    modelpath = './data/torch_single_layer.pth'
 
     train_path = os.path.join(filedir_in, 'train.pt')
     x_train = torch.load(train_path)
@@ -34,6 +35,8 @@ if __name__ == "__main__":
         
         if epoch % 10 == 0:
             print('epoch: {}, loss: {:.4f}'.format(epoch+1, loss))
+    torch.save(net.state_dict(), modelpath)
+    
     '''
     epoch: 1, loss: 0.5623
     epoch: 11, loss: 0.5619
