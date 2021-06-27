a7b1bda5a856b7bf74ea22c5248c151999bfeb09
taro-masuda <taro.masuda.jp@gmail.com>
2021-06-27 15:12:21 +0900

    [add] added an answer on 91.

--- ch10/load_dataset_90.sh
+++ ch10/load_dataset_90.sh
@@ -1,2 +1,1 @@
 onmt_build_vocab -config ch10/en_ja.yaml -n_sample 10000
-onmt_train -config ch10/en_ja.yaml
---
+++ ch10/train_machine_translation_model_91.sh
@@ -0,0 +1,5 @@
+onmt_train -config ch10/en_ja.yaml
+
+# [2021-06-27 06:08:11,038 INFO] Validation perplexity: 118.637
+# [2021-06-27 06:08:11,038 INFO] Validation accuracy: 27.176
+# [2021-06-27 06:08:11,365 INFO] Saving checkpoint data/run/en_ja_model_step_1000.pt
